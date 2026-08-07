226cb15f4f29ed996ad735b27aae0d88ba3c8173
DerAnsari <ansarisarmad426@gmail.com>
2026-08-07 18:51:45 +0500

    added treesitter plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+-- ~/.config/nvim/lua/plugins/treesitter.lua
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      -- A list of parser names you want installed automatically
+      ensure_installed = { 
+        "c", 
+        "cpp", 
+        "glsl", 
+        "lua", 
+        "rust",
+        "go",
+        "c-sharp",
+        "python",
+        "bash",
+        "markdown",
+        "markdown_inline",
+        "nix", 
+        "vim", 
+        "vimdoc", 
+        "query" 
+      },
+
+      -- Automatically install missing parsers when entering buffer
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+        -- Disables standard vim highlighting for these languages
+        additional_vim_regex_highlighting = false,
+      },
+      
+      indent = { enable = true },
+    })
+  end
+}
