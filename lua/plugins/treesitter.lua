75e0a9231e623b95862de1a0c0a156522094f9a7
DerAnsari <ansarisarmad426@gmail.com>
2026-08-07 19:14:56 +0500

    more more bugs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,27 @@
 -- ~/.config/nvim/lua/plugins/treesitter.lua
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" }, -- Lazy-load when opening a file
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  opts = {
-    ensure_installed = { 
-      "c", 
-      "cpp", 
-      "glsl", 
-      "lua", 
-      "nix", 
-      "vim", 
-      "vimdoc", 
-      "query", 
-      "rust",
-      "go",
-      "c_sharp",
-      "python",
-      "bash",
-      "markdown",
-      "markdown_inline",
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  config = function()
+    -- 1. Initialize treesitter
+    require('nvim-treesitter').setup({
+      install_dir = vim.fn.stdpath('data') .. '/site'
+    })
+
+    -- 2. Install your parsers (runs asynchronously)
+    require('nvim-treesitter').install({
+      'c', 'cpp', 'glsl', 'lua', 'nix', 
+      'rust', 'go', 'c_sharp', 'python', 'bash', 'markdown'
+    })
+
+    -- 3. Enable highlighting globally via Neovim's built-in FileType autocommand
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { '*' },
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end
 }
