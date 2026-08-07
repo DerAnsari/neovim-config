ed06e521dc8c51b6cc201b0639130069c781e27d
DerAnsari <ansarisarmad426@gmail.com>
2026-08-07 19:08:26 +0500

    more bugs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,39 +3,34 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      -- A list of parser names you want installed automatically
-      ensure_installed = { 
-        "c", 
-        "cpp", 
-        "glsl", 
-        "lua", 
-        "rust",
-        "go",
-        "c_sharp",
-        "python",
-        "bash",
-        "markdown",
-        "markdown_inline",
-        "nix", 
-        "vim", 
-        "vimdoc", 
-        "query" 
-      },
-
-      -- Automatically install missing parsers when entering buffer
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-        -- Disables standard vim highlighting for these languages
-        additional_vim_regex_highlighting = false,
-      },
-      
-      indent = { enable = true },
-    })
-  end
+  event = { "BufReadPre", "BufNewFile" }, -- Lazy-load when opening a file
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  opts = {
+    ensure_installed = { 
+      "c", 
+      "cpp", 
+      "glsl", 
+      "lua", 
+      "nix", 
+      "vim", 
+      "vimdoc", 
+      "query", 
+      "rust",
+      "go",
+      "c_sharp",
+      "python",
+      "bash",
+      "markdown",
+      "markdown_inline",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = { enable = true },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
