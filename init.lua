a24b7777dcf4cc42dbee690cee1e24babffbcfc6
DerAnsari <ansarisarmad426@gmail.com>
2026-08-07 17:45:44 +0500

    added the lazy plugin manager

--- init.lua
+++ init.lua
@@ -9,6 +9,25 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup Lazy and tell it to manage base16-nvim
+require("lazy").setup({
+  { "RRethy/base16-nvim" },
+})
+
 -- load Noctalia-generated colors if present
 local ok, matugen = pcall(require, "matugen")
 if ok then
