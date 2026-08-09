5c45067eeca5ac25faee4c1e10e70daf05b3eeab
DerAnsari <ansarisarmad426@gmail.com>
2026-08-10 00:10:43 +0500

    adding snacks.nvim for that juicy UI

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		dashboard = { enabled = true },
+		notifier = { enabled = true },
+		indent = { enabled = true },
+		words = { enabled = true },
+		bigfile = { enabled = true },
+	},
+}
