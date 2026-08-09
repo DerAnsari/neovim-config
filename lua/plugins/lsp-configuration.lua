82e17c3784834129867013437a8f82b2869e7bb3
DerAnsari <ansarisarmad426@gmail.com>
2026-08-09 21:06:42 +0500

    added keymaps

--- lua/plugins/lsp-configuration.lua
+++ lua/plugins/lsp-configuration.lua
@@ -12,6 +12,7 @@ return {
         "rust_analyzer",
         "clangd",
         "gopls",
+        "nil_ls",
         "pyright",
         "bashls",
         "yamlls",
@@ -26,6 +27,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      -- Enable LSPs
       vim.lsp.enable({
         "lua_ls",
         "rust_analyzer",
@@ -35,6 +37,32 @@ return {
         "pyright",
         "bashls",
         "yamlls",
+      })
+
+      -- LSP keymaps
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          local opts = { buffer = event.buf, silent = true }
+
+          -- Navigation
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+          -- Information
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+          -- Actions
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+          -- Diagnostics
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+        end,
       })
     end,
   },
