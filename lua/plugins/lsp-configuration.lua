85b52eb63ddc23a80362aa15a97aeeb3cc91b64a
DerAnsari <ansarisarmad426@gmail.com>
2026-08-09 21:47:15 +0500

    changing things cuz nix be like that

--- lua/plugins/lsp-configuration.lua
+++ lua/plugins/lsp-configuration.lua
@@ -6,17 +6,7 @@ return {
 
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "rust_analyzer",
-        "clangd",
-        "gopls",
-        "pyright",
-        "bashls",
-        "yamlls",
-      },
-    },
+    opts = {},
     dependencies = {
       "mason-org/mason.nvim",
       "neovim/nvim-lspconfig",
