f491468e3e71edc27e2fa46e8b861cb14a360aa7
DerAnsari <ansarisarmad426@gmail.com>
2026-08-09 23:55:20 +0500

    adding autocomplete plugin

--- lua/plugins/lsp-configuration.lua
+++ lua/plugins/lsp-configuration.lua
@@ -1,58 +1,56 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"saghen/blink.cmp",
+		},
+		config = function()
+			-- merge blink.cmp's capabilities into every LSP server
+			vim.lsp.config("*", {
+				capabilities = require("blink.cmp").get_lsp_capabilities(),
+			})
 
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-  },
+			-- Enable LSPs
+			vim.lsp.enable({
+				"lua_ls",
+				"rust_analyzer",
+				"clangd",
+				"gopls",
+				"nil_ls",
+				"pyright",
+				"bashls",
+				"yamlls",
+			})
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- Enable LSPs
-      vim.lsp.enable({
-        "lua_ls",
-        "rust_analyzer",
-        "clangd",
-        "gopls",
-        "nil_ls",
-        "pyright",
-        "bashls",
-        "yamlls",
-      })
-
-      -- LSP keymaps
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(event)
-          local opts = { buffer = event.buf, silent = true }
-
-          -- Navigation
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-
-          -- Information
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-
-          -- Actions
-          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-
-          -- Diagnostics
-          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-        end,
-      })
-    end,
-  },
+			-- LSP keymaps
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(event)
+					local opts = { buffer = event.buf, silent = true }
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+					vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+					vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				end,
+			})
+		end,
+	},
 }
