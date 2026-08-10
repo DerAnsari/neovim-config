6426f5f8d093ffc64b22f2b8c8d1258622e8152e
DerAnsari <ansarisarmad426@gmail.com>
2026-08-10 14:57:24 +0500

    more binds change

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,11 @@
 -- ~/.config/nvim/lua/config/options.lua
 
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
